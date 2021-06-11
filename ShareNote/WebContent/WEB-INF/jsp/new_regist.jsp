7149ed48f080a409a3e14e1e9258e216cb969745
doj02021-a-kobayashi <kobayashi-a@seplus2016.onmicrosoft.com>
2021-06-11 18:18:15 +0900

    New_regist.javaと、new_regist.jspを更新

--- ShareNote/WebContent/WEB-INF/jsp/new_regist.jsp
+++ ShareNote/WebContent/WEB-INF/jsp/new_regist.jsp
@@ -8,6 +8,49 @@
 <title>mynote++</title>
 </head>
 <body>
-
+<h2>ユーザー登録</h2>
+<form method="POST" action="/ShareNote/New_regist">
+	<table align="center">
+	    <tr>
+			<th>ニックネーム</th>
+			<td><input type="text" name="nickname" placeholder="重複不可"></td>
+			<td>${errMsg1}</td>
+		</tr>
+		<tr>
+			<th>パスワード</th>
+			<td><input type="password" name="password" placeholder="5文字以上16文字以内"></td>
+			<td>※半角英数字と記号のみ使用可</td>
+		</tr>
+		<tr>
+			<th>パスワード(確認)</th>
+			<td><input type="password" name="password_check"></td>
+			<td>${errMsg2}</td>
+		</tr>
+		<tr>
+			<th>秘密の質問</th>
+			<td>
+				<select name="question">
+					<option value="好きな食べ物は？">好きな食べ物は？</option>
+					<option value="初めて飼ったペットの名前は？">初めて飼ったペットの名前は？</option>
+					<option value="初めて行った海外は？">初めて行った海外は？</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<th>答え</th>
+			<td><input type="text" name="answer"></td>
+		</tr>
+		<tr>
+			<td colspan="2">${errMsg3}</td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center"><input type="submit" name="login" value="ログイン" ></td>
+		</tr>
+		<tr>
+			<td><a href="Login.java">ログイン画面に戻る</a></td>
+		</tr>
+	</table>
+</form>
+<jsp:include page="/WEB-INF/jsp/footer.jsp"/>
 </body>
 </html>
--- ShareNote/src/servlet/New_regist.java
+++ ShareNote/src/servlet/New_regist.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,13 +14,24 @@ public class New_regist extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		// ログインページにフォワード
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/new_regist.jsp");
+				dispatcher.forward(request, response);
+			}
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		String nickname = request.getParameter("nickname");
+		String password = request.getParameter("password");
+		String question = request.getParameter("question");
+		String answer = request.getParameter("answer");
+
+		// 新規登録処理
+
+		// 新規登録が成功
+		// 新規登録が失敗
 	}
 
 }
