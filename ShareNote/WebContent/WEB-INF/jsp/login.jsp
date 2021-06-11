4fb5c49df8805eadaa5bb82b85fdec75d1cb8df7
dojo2021-k-joko <joko@seplus2016.onmicrosoft.com>
2021-06-11 13:38:46 +0900

    ログインjsp

--- ShareNote/WebContent/WEB-INF/jsp/login.jsp
+++ ShareNote/WebContent/WEB-INF/jsp/login.jsp
@@ -8,6 +8,30 @@
 <title>mynote++</title>
 </head>
 <body>
-
+${msg}
+<h2>ログイン</h2>
+<form>
+	<table align="center">
+		<tr>
+			<th>ニックネーム</th>
+			<td><input type="text" name="nickname" placeholder="重複不可"></td>
+		</tr>
+		<tr>
+			<th>パスワード</th>
+			<td><input type="password" name="password" placeholder="5文字以上16文字以内"></td>
+		</tr>
+		<tr>
+			<td colspan="2">${errMsg}</td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" name="login" value="ログイン" ></td>
+		</tr>
+		<tr>
+			<td><a href="New_regist.java">新規登録</a></td>
+			<td><a href="Re_regist.java">パスワードを忘れた方</a></td>
+		</tr>
+	</table>
+</form>
+<jsp:include page="/WEB-INF/jsp/footer.jsp"/>
 </body>
 </html>
--- ShareNote/src/servlet/Login.java
+++ ShareNote/src/servlet/Login.java
@@ -3,6 +3,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,8 +15,9 @@ public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// ログインページにフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
