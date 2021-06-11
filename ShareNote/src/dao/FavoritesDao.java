a8b04e63e11d559f38d678e71ffff13b23df26bb
dojo2021-y-sugimori <sugimori@seplus2016.onmicrosoft.com>
2021-06-11 17:40:53 +0900

    DAOのコメント入れておきました。

--- ShareNote/src/dao/FavoritesDao.java
+++ ShareNote/src/dao/FavoritesDao.java
@@ -1,13 +1,14 @@
 //お気に入り登録関連のデータアクセス用DAO
 package dao;
 
-//JDBCの基本機能
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class FavoritesDao {
+
+//ノート詳細
+//こちらもおススメを表示する
+
+
+//ノートをお気に入り登録する
+
+
 
 }
