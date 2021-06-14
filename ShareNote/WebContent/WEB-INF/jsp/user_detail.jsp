30473ba6d37fe12eb2890035b7e082e63c30f846
r-funami <funami@seplus2016.onmicrosoft.com>
2021-06-14 17:46:59 +0900

    user_detail.jspとuse_detail.java

--- ShareNote/WebContent/WEB-INF/jsp/user_detail.jsp
+++ ShareNote/WebContent/WEB-INF/jsp/user_detail.jsp
@@ -8,6 +8,38 @@
 <title>mynote++</title>
 </head>
 <body>
+<jsp:include page="/WEB-INF/jsp/header.jsp"/>
 
+<!-- サイドメニュー -->
+<h2>${nickname}さんのマイページ</h2>
+
+
+	<form method="POST" action="/ShareNote/Note_detail">
+	<table border="1">
+		<tr>
+			<td rowspan="3">${e.image_files}ノート画像</td>
+			<td>
+			${e.year}年度
+			</td>
+			<td>
+			${e.nickname}ニックネーム
+			</td>
+			<td rowspan="2" align="center"><input type="submit" name="detail" value="詳細"></td>
+			</tr>
+			<tr>
+			<td colspan="2">${e.title}タイトル</td>
+			</tr>
+
+		<tr>
+			<td>${e.tag}タグ</td>
+			<td><input type="submit" name="download" value="ダウンロード"></td>
+			<td><input type="submit" name="favorite" value="★"></td>
+		</tr>
+		</table>
+		<hr>
+		</form>
+
+
+<jsp:include page="/WEB-INF/jsp/footer.jsp"/>
 </body>
 </html>
--- ShareNote/src/servlet/User_detail.java
+++ ShareNote/src/servlet/User_detail.java
@@ -3,6 +3,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,7 +19,10 @@ public class User_detail extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//フォワード
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/user_detail.jsp");
+				dispatcher.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
