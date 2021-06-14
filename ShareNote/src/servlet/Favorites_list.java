703ca718d667d33ba537d0af8a9c918283ab6424
dojo2021-y-kageyama <kageyama-y@seplus2016.onmicrosoft.com>
2021-06-14 14:24:10 +0900

    お気に入り一覧jsp・サーブレット　途中まで

--- ShareNote/src/servlet/Favorites_list.java
+++ ShareNote/src/servlet/Favorites_list.java
@@ -3,24 +3,29 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Favorites_list")
 public class Favorites_list extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/ShareNote/Login");
+			return;
 	}
+		// お気に入り一覧ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/favorites_list.jsp");
+				dispatcher.forward(request, response);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
-
 }
+
