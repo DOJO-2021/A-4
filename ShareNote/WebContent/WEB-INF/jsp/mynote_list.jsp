793390cd4b90828d0e5fc8eafb0d404970bd81e5
dojo2021-y-sugimori <sugimori@seplus2016.onmicrosoft.com>
2021-06-14 14:26:40 +0900

    途中経過

--- ShareNote/WebContent/WEB-INF/jsp/mynote_list.jsp
+++ ShareNote/WebContent/WEB-INF/jsp/mynote_list.jsp
@@ -8,6 +8,55 @@
 <title>mynote++</title>
 </head>
 <body>
+<jsp:include page="/WEB-INF/jsp/header.jsp"/>
+
+<!-- サイドメニュー
+<h2>${nickname}さんのマイページ</h2>
+<ul>
+	<li><a href="Note_uplode">ノートをアップロード</a></li>
+</ul>
+<ul>
+	<li><a href="Mynote_list">マイノート一覧</a></li>
+</ul>
+<ul>
+	<li><a href="Favorites_list">お気に入り一覧</a></li>
+</ul>
+<ul>
+	<li></li>
+</ul>
+<ul>
+	<li><a href="Logout">ログアウト</a></li>
+</ul>
+-->
+<p>マイノート一覧</p>
+
+	<!-- 試しにforの外に書いてみる -->
+	<form method="POST" action="/ShareNote/Edit">
+	<table border="1">
+	<tr>
+		<td rowspan="3">${e.image_files}ノート画像</td>
+		<td>${e.year}年度</td>
+		<td rowspan="2">${e.title}タイトル</td>
+		<td rowspan="2" align="center"><input type="submit" name="edit" value="編集"></td>
+	</tr>
+	<tr>
+		<td>${favorites_num}お気に入り数</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">${e.tag}タグ</td>
+		<td><input type="submit" name="download" value="ダウンロード"></td>
+	</tr>
+	</table>
+	</form>
+	<!-- 試しにforの外に書いてみるここまで -->
+
+<c:forEach var="e" items="${cardList}" >
+	<form method="POST" action="/ShareNote/Edit">
+
+	</form>
+
+</c:forEach>
+
 
 </body>
 </html>
--- ShareNote/src/servlet/Note_upload.java
+++ ShareNote/src/servlet/Note_upload.java
@@ -2,24 +2,41 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Note_upload")
 public class Note_upload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/ShareNote/Login");
+			return;
+		}
+
+		// ノートのアップロードページをインクルードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/note_upload.jsp");
+		dispatcher.forward(request, response);
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/ShareNote/Login");
+			return;
+		}
 	}
-
 }
