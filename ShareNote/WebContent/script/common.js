43388b8af9defa058550fa12536ac67bf67363a3
r-funami <funami@seplus2016.onmicrosoft.com>
2021-06-18 16:37:44 +0900

    javaScript完成

--- ShareNote/WebContent/script/common.js
+++ ShareNote/WebContent/script/common.js
@@ -1,3 +1,34 @@
-/**
- * 
- */
+'use strict';
+
+
+
+
+function onRegist(){
+
+		var nickname = document.inform.nickname.value.trim();
+		var password = document.inform.password.value.trim();
+		var passconf = document.inform.password_check.value.trim();
+		var answer = document.inform.answer.value.trim();
+		//エラーメッセージを表示する場所の情報を取得
+		var target = document.getElementById("errMsg");
+
+		if(nickname=="" || password=="" || passconf=="" || answer==""){
+			target.innerHTML = "入力されていない項目があります";
+			return false;
+		}else{
+
+			if(window.confirm("この内容で登録してよろしいですか？")){
+
+				//OKのときの処理
+				return true;
+
+			}else{
+				//キャンセルの時の処理
+				return false;
+			}
+		}
+
+
+};
+
+
